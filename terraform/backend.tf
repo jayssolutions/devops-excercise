141b1068242ba26ed6b98a25618b17861f831a78
jayssolutions <jayssolutions69@gmail.com>
2026-09-11 13:33:04 +0530

    ansible config and pipeline updates

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket       = "jays-07092026-875285643708-us-east-1-an"
+    bucket       = "jays-07092026-875285643708"
     key          = "excercise/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
