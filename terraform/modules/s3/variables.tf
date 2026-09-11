97c2208981e78f297ec5e76f86322f2fb1fe61cb
jayssolutions <jayssolutions69@gmail.com>
2026-09-11 12:44:42 +0530

    added terraform configs

---
+++ terraform/modules/s3/variables.tf
@@ -0,0 +1,14 @@
+variable "bucket_name" { type = string}
+
+variable "force_destroy" {
+  type        = bool
+  default     = false
+  description = "If true, allows deleting the S3 bucket even if it contains state files"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags to apply to the S3 bucket"
+}
+
