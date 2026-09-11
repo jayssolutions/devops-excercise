53685cf94fecc989d468e928acf6bd9be86883fb
jayssolutions <jayssolutions69@gmail.com>
2026-09-11 19:04:03 +0530

    Revert "updated prometheus configs"

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,26 +45,6 @@ resource "aws_security_group_rule" "prometheus_scrape_app" {
   description              = "Prometheus scraping app /metrics"
 }
 
-module "logging" {
-  source            = "./modules/logging"
-  project_name      = var.project_name
-  vpc_id            = module.network.vpc_id
-  public_subnet_ids = module.network.public_subnet_ids
-  admin_cidr        = var.admin_cidr
-  public_key        = var.public_key
-}
-
-# Allow app instances to ship logs (via Filebeat) to Logstash
-resource "aws_security_group_rule" "logstash_ingest_from_app" {
-  type                     = "ingress"
-  from_port                = 5044
-  to_port                  = 5044
-  protocol                 = "tcp"
-  security_group_id        = module.logging.logging_security_group_id
-  source_security_group_id = module.compute.app_security_group_id
-  description              = "Filebeat shipping app logs to Logstash"
-}
-
 # Create the S3 State Bucket
 module "state" {
   source        = "./modules/s3"
--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -7,8 +7,3 @@ output "instance_private_ips" {
   description = "Private IP addresses of application instances"
   value       = aws_instance.app[*].private_ip
 }
-
-output "app_security_group_id" {
-  description = "Security group ID attached to the application instances"
-  value       = aws_security_group.app.id
-}
