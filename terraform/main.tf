baa0e3120df2e0d9b9122197b94034d5d019bc02
jayssolutions <137058390+jayssolutions@users.noreply.github.com>
2026-09-13 14:40:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,28 +22,7 @@ module "compute" {
   instance_count     = var.instance_count
   admin_cidr         = var.admin_cidr
   public_key         = var.public_key
-}
-
-module "monitoring" {
-  source            = "./modules/monitoring"
-  project_name      = var.project_name
-  vpc_id            = module.network.vpc_id
-  public_subnet_ids = module.network.public_subnet_ids
-  admin_cidr        = var.admin_cidr
-  public_key        = var.public_key
-  app_targets       = [for ip in module.compute.instance_private_ips : "${ip}:3000"]
 } 
-
-# Allow Prometheus to scrape the app instances' /metrics endpoint
-resource "aws_security_group_rule" "prometheus_scrape_app" {
-  type                     = "ingress"
-  from_port                = 3000
-  to_port                  = 3000
-  protocol                 = "tcp"
-  security_group_id        = module.compute.app_security_group_id
-  source_security_group_id = module.monitoring.prometheus_security_group_id
-  description              = "Prometheus scraping app /metrics"
-}
 
 # Create the S3 State Bucket
 module "state" {
