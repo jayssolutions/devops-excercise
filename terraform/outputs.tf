f06f23f94599d444f4db2b61218108d3fc68034f
jayssolutions <137058390+jayssolutions@users.noreply.github.com>
2026-09-13 14:41:45 +0530

    Remove unused output variables from outputs.tf
    Removed unused output variables for monitoring and logging.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,14 +6,3 @@ output "instance_public_ips" {
   value = module.compute.instance_public_ips
 }
 
-output "prometheus_url" {
-  value = module.monitoring.prometheus_url
-}
-
-output "logging_public_ip" {
-  value = module.logging.logging_public_ip
-}
-
-output "kibana_url" {
-  value = module.logging.kibana_url
-}
